61eb3e42096cef38395680d7f46d9387557115ce
Jagannath <jagannath.bhanja@ioptechnologies.com>
2026-09-15 09:21:02 +0530

    added new database

--- krishnaTyreApi/settings.py
+++ krishnaTyreApi/settings.py
@@ -94,23 +94,23 @@ WSGI_APPLICATION = 'krishnaTyreApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "myapp_db",
-#         "USER": "myapp_user",
-#         "PASSWORD": "KrishnatyreDB@2026",
-#         "HOST": "4.240.113.62",
-#         "PORT": "5432",
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "myapp_db",
+        "USER": "myapp_user",
+        "PASSWORD": "KrishnatyreDB@2026",
+        "HOST": "4.240.113.62",
+        "PORT": "5432",
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
